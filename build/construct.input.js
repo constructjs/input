3f2b6bde9c6b8c4793b64f4cc98e6104d6749110
Makis Tracend <makis.tracend@gmail.com>
2013-07-01 05:01:07 -0700

    using Object.extend

--- build/construct.input.js
+++ build/construct.input.js
@@ -1,56 +1,56 @@
 (function(){
 	// exit now if contruct hasn't already been defined
 	if(typeof construct == "undefined") return;
-	
+
 	construct.input = function( options ){
 		// options is an array...
 		//console.log(options);
-		
+
 		// lookup options
 		if( options.indexOf("keys") > -1 ) construct.config.deps.push("backbone.keys");
 		if( options.indexOf("mouse") > -1 ) construct.config.deps.push("backbone.mouse");
 		if( options.indexOf("touch") > -1 ) construct.config.deps.push("backbone.touch");
 		if( options.indexOf("gamepad") > -1 ) construct.config.deps.push("gamepad");
-		
+
 		return function( e ){
 			console.log( "update" );
 		};
-		
+
 	};
-	
+
 	// Dependencies
-	construct.config = $.extend( true, construct.config, {
+	construct.config = Object.extend(construct.config, {
 		"paths": {
 			"backbone.keys" : [
 				"//raw.github.com/constructjs/backbone.keys/master/backbone.keys"
-			], 
+			],
 			"backbone.touch" : [
 				"//raw.github.com/constructjs/backbone.touch/master/backbone.touch"
-			], 
+			],
 			"backbone.mouse" : [
 				//"https://gist.github.com/raw/4333890/9b8eebf4e1594d79760485eb818fd292691bc8c8/backbone.mouse",
 				"lib/backbone.mouse"
-			], 
+			],
 			"gamepad" : [
 				"//raw.github.com/constructjs/gamepad.js/master/gamepad"
-			] 
-		}, 
+			]
+		},
 		"shim": {
 			"backbone.keys": {
 				"deps": [
 					"backbone",
-					"underscore", 
+					"underscore",
 					"jquery"
 				]
-			}, 
+			},
 			"backbone.touch": {
 				"deps": [
 					"backbone",
-					"underscore", 
+					"underscore",
 					"jquery"
 				]
-			} 
+			}
 		}
 	});
-	
+
 })();

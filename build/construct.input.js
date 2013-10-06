a4728bba7694bb60d1b80cd8da13fffc50c2f25a
Makis Tracend <makis.tracend@gmail.com>
2013-10-06 02:24:07 -0700

    adding clickObject method

--- build/construct.input.js
+++ build/construct.input.js
@@ -71,11 +71,16 @@ construct.promise.add(function(){
 
 
 // in case APP.Mesh has already been defined by a plugin
-	var Mesh = APP.Mesh || View;
+	var Main3D = APP.Views.Main3D || APP.View;
 
-	APP.Mesh = Mesh.extend({
+	APP.Views.Main3D = Main3D.extend({
+
+		options: {
+			monitorMove: true
+		},
+
 		initialize: function( options ){
-			var types = construct.options.input;
+			var types = construct.options.input || [];
 			// lookup input types
 			if( types.indexOf("keys") > -1 ) {
 			}
@@ -86,7 +91,50 @@ construct.promise.add(function(){
 			if( types.indexOf("gamepad") > -1 ){
 			}
 
-			return Mesh.prototype.initialize.call(this, options);
+			this.on("intersect", _.bind(this.clickObject, this));
+
+			return Main3D.prototype.initialize.call(this, options);
+		},
+
+		_start: function( $3d ){
+			// create a projector for the click events
+			$3d.projector = new THREE.Projector();
+			return Main3D.prototype._start.call(this, $3d);
+		},
+
+		mousedown: function(){
+
+			var $el = $(this.el),
+				elOffset = $el.offset(),
+				mouse = this.params.get("mouse");
+			// check click event with a raycast
+			var x = ( (mouse.x - elOffset.left) / jQuery(window).width() ) * 2 - 1;
+			var y = - ( (mouse.y - elOffset.top) / jQuery(window).height() ) * 2 + 1;
+			var z = 0.5;
+			//
+			var vector = new THREE.Vector3( x, y, z );
+			var camera = this.$3d.active.camera;
+			// get the objects (map as array...)
+			var objects = $.map(this.$3d.objects, function (value, key) { return (value.children.length) ? value.children[0] : value; });
+			//this.$3d
+			this.$3d.projector.unprojectVector( vector, camera );
+
+			var raycaster = new THREE.Raycaster( camera.position, vector.sub( camera.position ).normalize() );
+
+			var intersects = raycaster.intersectObjects( objects );
+
+			if ( intersects.length > 0 ) {
+				// is the array always with one item and does the object always have a parent (assuming it's an Object3D container)?
+				var object = intersects[0].object.parent;
+				this.trigger("intersect", object);
+				//intersects[0].object.parent.lookAt(new THREE.Vector3( 0, 0, 0 ) );
+				// console.log( JSON.stringify( intersects[0].object.parent.rotation ) );
+			}
+
+		},
+
+		clickObject: function( objects ){
+			console.log("clickObject:", objects);
 		}
 
 	});

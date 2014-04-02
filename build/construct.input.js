1284cf64a93825c865e968e93826d79bd9963c3c
Makis Tracend <makis.tracend@gmail.com>
2014-04-01 18:29:59 -0700

    :zzz: tweaking axis order

--- build/construct.input.js
+++ build/construct.input.js
@@ -210,15 +210,17 @@ construct.promise.add(function(){
 			// support only first gamepad for now...
 			if( data && data[0] ){
 				var axes = data[0].axes;
-				// convention: first axes is left, second is right
-				this.state.move.forward = ( axes[1] < -0.4 ) ? 1 : 0;
-				this.state.move.back = ( axes[1] > 0.4 ) ? 1 : 0;
+				// convention: two first axes is left, two latter is right
 				this.state.move.left = ( axes[0] < -0.4 ) ? 1 : 0;
 				this.state.move.right = ( axes[0] > 0.4 ) ? 1 : 0;
+				this.state.move.forward = ( axes[1] < -0.4 ) ? 1 : 0;
+				this.state.move.back = ( axes[1] > 0.4 ) ? 1 : 0;
+
 				this.state.move.yawLeft = ( axes[2] < -0.4 ) ? 1 : 0;
 				this.state.move.yawRight = ( axes[2] > 0.4 ) ? 1 : 0;
 				this.state.move.pitchUp = ( axes[3] < -0.4 ) ? 1 : 0;
 				this.state.move.pitchDown = ( axes[3] > 0.4 ) ? 1 : 0;
+
 			}
 
 			this.updateMovementVector();

9e0f4afe5fa3f5c6178aaf9e1f659975b241c0fd
Makis Tracend <makis.tracend@gmail.com>
2014-04-04 01:07:49 -0700

    using bleeding edge versions of backbone.input deps

--- build/construct.input.js
+++ build/construct.input.js
@@ -34,16 +34,16 @@
 	construct.config = Object.extend(construct.config, {
 		"paths": {
 			"backbone.ui.keys" : [
-				"//rawgithub.com/backbone-input/keys/0.3.0/dist/backbone.keys.min"
+				"//rawgithub.com/backbone-input/keys/master/build/backbone.input.keys"
 			],
 			"backbone.ui.touch" : [
-				"//rawgithub.com/backbone-input/touch/0.3.0/dist/backbone.touch.min"
+				"//rawgithub.com/backbone-input/touch/master/build/backbone.input.touch"
 			],
 			"backbone.ui.mouse" : [
-				"//rawgithub.com/backbone-input/mouse/master/backbone.input.mouse"
+				"//rawgithub.com/backbone-input/mouse/master/build/backbone.input.mouse"
 			],
 			"backbone.ui.gamepad" : [
-				"//rawgithub.com/backbone-input/gamepad/master/backbone.input.gamepad"
+				"//rawgithub.com/backbone-input/gamepad/master/build/backbone.input.gamepad"
 			]
 		},
 		"shim": {
